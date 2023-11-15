49a555ff1937ffa379a46fb003ce24e07a121b42
Roy <cc711612@gmail.com>
2023-11-15 20:35:37 +0800

    帳本新增幣別

--- app/Http/Requesters/Apis/Wallets/WalletUpdateRequest.php
+++ app/Http/Requesters/Apis/Wallets/WalletUpdateRequest.php
@@ -20,6 +20,7 @@ class WalletUpdateRequest extends Request
             'wallets.id'      => null,
             'wallets.user_id' => null,
             'wallets.title'   => null,
+            'wallets.unit'    => 'TWD',
             'wallets.status'  => 1,
         ];
     }
@@ -38,6 +39,7 @@ class WalletUpdateRequest extends Request
             'wallets.id'      => Arr::get($row, 'wallet'),
             'wallets.user_id' => Arr::get($row, 'user.id'),
             'wallets.title'   => Arr::get($row, 'title'),
+            'wallets.unit'    => Arr::get($row, 'unit'),
             'wallets.status'  => Arr::get($row, 'status'),
         ];
     }
